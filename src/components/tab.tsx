8cb91f0932c406d4fa8afff5a3e8a0ac6581459a
Nam Le <yentth237@gmail.com>
2023-05-16 14:51:19 +0700

    feat: add logo for the app

--- src/components/tab.tsx
+++ src/components/tab.tsx
@@ -1,11 +1,12 @@
+import Image from 'next/image';
+
 export default function Tab() {
 	return (
 		<div className="w-1/5 h-full">
-			<ol>
-				<li>
-					<p>AI chat</p>
-				</li>
-			</ol>
+			<div className="flex">
+				<Image src={'./logo-app.svg'} height={33} width={33} alt="logo" />
+				<p className="text-2xl font-bold ml-1 text-[#64BD64]">Super Chat</p>
+			</div>
 		</div>
 	);
 }
